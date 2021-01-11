22f2d41236f36a316c6445bde4e31410230621bc
Itzik Gili <itzik.gili@gmail.com>
2021-01-11 21:17:38 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express Socket App' });
+  res.send('yeee');
 });
 
 module.exports = router;
