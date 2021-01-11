336ffda54be97ff282b1f21fca8c19fe1b12a486
Itzik Gili <itzik.gili@gmail.com>
2021-01-11 21:37:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ const INDEX = '/index.html';
 let app = express()
 
 
-/* ROUTES */
-app.use('/', function(req, res) {
-  res.send('yeee');
-});
-
-
 const server = app
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
@@ -26,5 +20,13 @@ io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
+
+/* ROUTES */
+app.use('/', function(req, res) {
+  io.emit('msg','123);
+});
+
+
 
 // setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
